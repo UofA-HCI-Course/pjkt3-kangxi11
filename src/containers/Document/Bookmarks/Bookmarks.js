b5e4d4401efbc69598656d9a35a5c2713f618efb
Ryan Kang <rkang2009@gmail.com>
2022-03-23 21:01:05 -0600

    Added bookmarks file

--- src/containers/Document/Bookmarks/Bookmarks.js
+++ src/containers/Document/Bookmarks/Bookmarks.js
@@ -9,7 +9,7 @@ export default function Bookmarks(props) {
         <div>
             {props.bookmarks.map((bookmark, index) => (
                 <Grid item xs={2} sm={4} md={4} key={index}>
-                    <Typography variant="h6">{index}. Page 1</Typography>
+                    <Typography variant="h6">{index+1}. Page 1</Typography>
                     <Divider />
                     <div>
                         <Typography variant="body1" className="bookmarks-item" onClick={props.onClick}>
